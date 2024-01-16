6c33de476838d090fa8547a4c0cfa9d7495a542c
sjquant <seonujang92@gmail.com>
2024-01-17 00:54:43 +0900

    Mark up main page

---
+++ src/components/main/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+const PROFILE_IMAGE_LINK =
+  'https://dthezntil550i.cloudfront.net/ga/latest/ga1802220203146420002184481/0fb570cb-0202-443a-8da6-694740860342.png'
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+  background-color: #ffffff;
+  user-select: none;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React, { FunctionComponent } from 'react'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import GlobalStyle from '../components/common/GlobalStyle'
+import Introduction from '../components/main/Introduction'
+import Footer from '../components/common/Footer'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Link to="/info/">To Info</Link>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <Footer />
+    </Container>
   )
 }
 
