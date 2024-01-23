abb664be7f7613fede26943234974a864f9f27b3
sjquant <seonujang92@gmail.com>
2024-01-24 00:03:18 +0900

    Create url slug field

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,16 +3,28 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
+    path: '/using-dsg',
+    component: require.resolve('./src/templates/using-dsg.js'),
     context: {},
     defer: true,
   })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({ node, name: 'slug', value: slug })
+  }
 }
