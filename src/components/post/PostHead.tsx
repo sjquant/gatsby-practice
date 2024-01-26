dc1647b1e8f0bf0034c9f0c82814da6e54770600
sjquant <seonujang92@gmail.com>
2024-01-27 00:24:51 +0900

    Make it responsive

--- src/components/post/PostHead.tsx
+++ src/components/post/PostHead.tsx
@@ -20,6 +20,10 @@ const PostHeadWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const BackgroundImage = styled((props: GatsbyImgProps) => (
@@ -30,6 +34,10 @@ const BackgroundImage = styled((props: GatsbyImgProps) => (
   height: 400px;
   object-fit: cover;
   filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const PostHead: FunctionComponent<PostHeadProps> = function ({
